34695daef999b0883d7c7b0aad6d12fdec696fa5
preintercede <dylanw@uci.edu>
2021-07-06 18:01:06 -0700

    added buttons and imagescreen + image component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({navigation}) => {
+  return (
+  <View>
+    <Text style={styles.text}>HomeScreen</Text>
+    <Button 
+    onPress={()=> navigation.navigate('Components')}
+    title="Go to Components Demo"
+    />
+
+    <Button
+    title="Go to List Demo"
+    onPress={()=> navigation.navigate('List')}
+    />
+
+    <Button
+    title="Go to Image Demo"
+    onPress={()=> navigation.navigate('Image')}
+    />
+
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
